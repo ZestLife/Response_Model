7780c6c8b829e0c523dd9255222061467b385d25
Jadon Thomson <jadon.thomson@zestlife.co.za>
2016-06-24 08:42:47 +0200

    Outline

--- main.R
+++ main.R
@@ -1,3 +1,22 @@
-# Response Model
-# Jadon Thomson
-# 24/06/2016
+##  Response Model
+##  Jadon Thomson
+##  24/06/2016
+
+### Initialize Project
+
+
+
+### Import Data
+
+
+
+### Clean Data
+
+
+
+### Fit model
+
+
+
+
+### Get results
